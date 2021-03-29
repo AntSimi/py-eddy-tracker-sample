615dd9e9a70681fa4f2eea18b18ac1d572f7178c
Antoine Delepoulle <adelepoulle@cls.fr>
2021-03-29 11:14:17 +0200

    Add versioneer

--- py_eddy_tracker_sample/__init__.py
+++ py_eddy_tracker_sample/__init__.py
@@ -1,6 +1,11 @@
 from os import path
 
 
-def get_path(name):
+def get_demo_path(name):
     return path.join(path.dirname(__file__), name)
 
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+import versioneer
 
 setup(
     name="pyEddyTrackerSample",
-    version="0.0.0",
     description="Py-Eddy-Tracker samples",
     keywords="eddy samples",
     author="Antoine Delepoulle",
     author_email="delepoulle.a@gmail.com",
     packages=find_packages(),
     zip_safe=False,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     package_data={
         "py_eddy_tracker_sample": [
             "*/*.zarr/*/*",
@@ -18,4 +21,3 @@ setup(
         ],
     },
 )
-
